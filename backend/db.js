43e0edd331e14789899389d9321f46192e444841
Mitali Singh <107592707+Mitali77-tech@users.noreply.github.com>
2025-12-19 03:14:07 +0530

    Updated db.js

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Liveonedge@123",
+    password: "DB_PASSWORD",
     database: "equipment_db"
 });
 
